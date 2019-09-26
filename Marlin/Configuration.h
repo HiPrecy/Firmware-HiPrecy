9e2b87f0037d4b61a70ace618dcf0ab40c12e14d
GiliFuuu <GiliFuuu@outlook.com>
2019-09-26 14:30:31 +0800

    change default config for HiPrecy LEO

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -775,7 +775,7 @@
  */
 #define X_PROBE_OFFSET_FROM_EXTRUDER 35  // X offset: -left  +right  [of the nozzle]
 #define Y_PROBE_OFFSET_FROM_EXTRUDER 2  // Y offset: -front +behind [the nozzle]
-#define Z_PROBE_OFFSET_FROM_EXTRUDER -1.5   // Z offset: -below +above  [the nozzle]
+#define Z_PROBE_OFFSET_FROM_EXTRUDER -0.5   // Z offset: -below +above  [the nozzle]
 
 // Certain types of probes need to stay away from edges
 #define MIN_PROBE_EDGE 10
