a29021d762daa6012512670b9cabefeb0f3c82d0
GiliFuuu <GiliFuuu@outlook.com>
2019-10-26 14:13:05 +0800

    add condition for homing backward first function

--- Marlin/Marlin_main.cpp
+++ Marlin/Marlin_main.cpp
@@ -3362,8 +3362,17 @@ static void homeaxis(const AxisEnum axis) {
     if (axis == Z_AXIS && set_bltouch_deployed(true)) return;
   #endif
 
-  if (axis == X_AXIS || axis == Y_AXIS)
-    do_homing_move(axis, 3.0f * -axis_home_dir);
+  #if ENABLED(SENSORLESS_HOMING)
+    #if ENABLED(X_HOMING_SENSITIVITY)
+      if (axis == X_AXIS)
+        do_homing_move(axis, 3.0f * -axis_home_dir);
+    #endif
+
+    #if ENABLED(Y_HOMING_SENSITIVITY)
+      if (axis == Y_AXIS)
+        do_homing_move(axis, 3.0f * -axis_home_dir);
+    #endif
+  #endif
 
   do_homing_move(axis, 1.5f * max_length(axis) * axis_home_dir);
 
